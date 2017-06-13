0fe081ff8be98b90058053cf38fad79654cd273a
satyam tiwari <satyamcat2010@gmail.com>
2017-06-13 13:14:59 +0530

    My peoject setup completed successfully

---
+++ ssss/src/main/java/com/sat/ssss/App.java
@@ -0,0 +1,13 @@
+package com.sat.ssss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
