bacb42bdb017848fd1508ea004bebe47a8f7d3a7
satyam tiwari <satyamcat2010@gmail.com>
2017-06-23 08:15:37 +0530

    Theme interface and other issue fixed.

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -15,10 +15,37 @@ public class PageController {
 	{
 		ModelAndView mv=new ModelAndView("page");
 		
-		mv.addObject("greeting", "Welcome to Spring web mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome",true);
 		
 		return mv;
 	}
+
+	 
+  @RequestMapping(value= "/about")
+	
+	public ModelAndView about()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		
+		mv.addObject("title", "About us");
+		mv.addObject("userClickAbout",true);
+		
+		return mv;
+	}
+  
+  @RequestMapping(value= "/contact")
+	
+	public ModelAndView contact()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		
+		mv.addObject("title", "Contact us");
+		mv.addObject("userClickContact",true);
+		
+		return mv;
+	}
+
 
 	
 	
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,18 @@
+$(function() {
+	
+	switch(menu){
+	
+	
+	case 'About us':
+		$('#about').addClass('active');
+		break;
+	
+	case 'Contact us':
+		$('#contact').addClass('active');
+		break;
+		
+	default:
+		$('#home').addClass('active');
+	      break;	
+	}	
+});
